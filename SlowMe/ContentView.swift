1c09f480322d0f42b9641cb18fd299d6777fd7be
Chloe Lee <yeseo4615@gmail.com>
2026-01-30 11:41:24 +1100

    Initial Commit

---
+++ SlowMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlowMe
+//
+//  Created by Chloe Lee on 30/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlowMe/SlowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowMeApp.swift
+//  SlowMe
+//
+//  Created by Chloe Lee on 30/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct SlowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
